6a28ef8ad2d36c93ff5033e4137bc10c0b13ce46
GyuHyeong Nam <sjarbgud@gmail.com>
2021-12-04 05:22:20 +0900

    백준 10718번 We Love kriii

---
+++ src/백준/WeLoveKriii.java
@@ -0,0 +1,10 @@
+package 백준;
+
+// 백준 10718번 We Love kriii
+class WeLoveKriii {
+    public static void main(String[] args) {
+        String str = "강한친구 대한육군";
+        System.out.println(str);
+        System.out.println(str);
+    }
+}
