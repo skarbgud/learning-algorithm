e4501d245175c45b1b715889faf19ac1d26f6d59
skarbgud <54926902+skarbgud@users.noreply.github.com>
2021-12-08 10:42:49 +0900

    백준 1001번 A-B

---
+++ src/백준/AsubB.java
@@ -0,0 +1,19 @@
+package 백준;
+
+import java.util.Scanner;
+
+// 백준 1001번 A-B
+class AsubB {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+
+    System.out.println(A-B);
+
+    sc.close();
+  }
+}
+
+
+
