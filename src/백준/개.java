da07060ad14018f91efcf0d1982c6f63934c113d
skarbgud <54926902+skarbgud@users.noreply.github.com>
2021-12-08 10:37:06 +0900

    백준 10172번 개

---
+++ src/백준/개.java
@@ -0,0 +1,15 @@
+package 백준;
+
+// 백준 10172번 개
+class 개 {
+  public static void main(String[] args){
+    System.out.println("|\\_/|");
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )\"\"\"\\");
+    System.out.println("|\"^\"`    |");
+    System.out.println("||_/=\\\\__|");
+  }
+}
+
+
+
