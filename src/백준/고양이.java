138809d8a5adf70a7cc83d121b6211781a0c802e
skarbgud <54926902+skarbgud@users.noreply.github.com>
2021-12-08 10:28:05 +0900

    백준 10171번 고양이

---
+++ src/백준/고양이.java
@@ -0,0 +1,14 @@
+package 백준;
+
+// 백준 10171번 고양이
+class 고양이 {
+  public static void main(String[] args){
+      System.out.println("\\    /\\");
+      System.out.println(" )  ( ')");
+      System.out.println("(  /  )");
+      System.out.println(" \\(__)|");   
+  }
+}
+
+
+
