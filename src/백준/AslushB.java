c9ba66d3ef58e93bc5839d637e07fd8da30a7394
skarbgud <54926902+skarbgud@users.noreply.github.com>
2021-12-09 09:11:39 +0900

    백준 1008번 A/B

---
+++ src/백준/AslushB.java
@@ -0,0 +1,18 @@
+package 백준;
+
+import java.util.Scanner;
+
+// 백준 1008번 A/B
+class AslushB {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    Double A = sc.nextDouble();
+    Double B = sc.nextDouble();
+    System.out.println(A/B);
+
+    sc.close();
+  }
+}
+
+
+
