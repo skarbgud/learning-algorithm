addfcf9966bc6eb5fe004c2ecf87f6bf87bef71f
skarbgud <54926902+skarbgud@users.noreply.github.com>
2021-12-16 12:22:05 +0900

    백준 2438번 별 찍기 - 1

---
+++ src/백준/별찍기1.java
@@ -0,0 +1,27 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 백준 2438번 별 찍기 - 1
+class 별찍기1 {
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    br.close();
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= i; j++)
+      {
+        sb.append("*");
+      }
+      sb.append("\n");
+    }
+    
+    System.out.println(sb);
+  }
+}
