49664b9526728fab9c8b94b3f7e78206c6399af0
skarbgud <54926902+skarbgud@users.noreply.github.com>
2021-12-16 09:09:44 +0900

    백준 2741번 N찍기

---
+++ src/백준/N찍기.java
@@ -0,0 +1,29 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 백준 2741번 N찍기
+class N찍기 {
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    int n = Integer.parseInt(br.readLine());
+
+    for (int i = 1; i <= n; i++)
+    {
+      bw.write(i + "\n");
+    }
+    br.close();
+
+    bw.flush();
+    bw.close();
+  }
+}
+
+
+
