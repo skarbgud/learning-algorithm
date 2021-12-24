6113ae5347ad483dbfc9071e8d7e0274801af71d
GyuHyeong Nam <sjarbgud@gmail.com>
2021-12-25 02:11:31 +0900

    백준 5622번 다이얼

---
+++ src/백준/다이얼.java
@@ -0,0 +1,67 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 백준 5622번 다이얼
+class 다이얼 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+
+        br.close();
+
+        int count = 0;
+
+        for (char i = 0; i < word.length(); i++) {
+            switch (word.charAt(i)) {
+                case 'A':
+                case 'B':
+                case 'C':
+                    count += 3;
+                    break;
+                case 'D':
+                case 'E':
+                case 'F':
+                    count += 4;
+                    break;
+                case 'G':
+                case 'H':
+                case 'I':
+                    count += 5;
+                    break;
+                case 'J':
+                case 'K':
+                case 'L':
+                    count += 6;
+                    break;
+                case 'M':
+                case 'N':
+                case 'O':
+                    count += 7;
+                    break;
+                case 'P':
+                case 'Q':
+                case 'R':
+                case 'S':
+                    count += 8;
+                    break;
+                case 'T':
+                case 'U':
+                case 'V':
+                    count += 9;
+                    break;
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
+                    count += 10;
+                    break;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
