b044aa9cf02600b4da7598feae827f7f7a16e033
GyuHyeong Nam <sjarbgud@gmail.com>
2021-12-30 00:21:25 +0900

    백준 10757번 큰 수 A+B

---
+++ src/백준/큰수AplusB.java
@@ -0,0 +1,22 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+// 백준 10757번 큰 수 A+B
+class 큰수AplusB {
+    public static void main(String[] args) throws IOException {
+
+//        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine() + " ");
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        System.out.println(A.add(B));
+    }
+}
