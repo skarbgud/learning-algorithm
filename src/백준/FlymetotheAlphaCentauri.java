e0f115e8b24730f2adb9b4895844486dcd9228d2
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-01 04:27:24 +0900

    백준 1011번 Fly me to the Alpha Centauri

---
+++ src/백준/FlymetotheAlphaCentauri.java
@@ -0,0 +1,36 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 1011번 Fly me to the Alpha Centauri
+class FlymetotheAlphaCentauri {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine() + " ");
+
+            int X = Integer.parseInt(st.nextToken());
+            int Y = Integer.parseInt(st.nextToken());
+
+            int distance = Y-X;
+            int max = (int)Math.sqrt(distance);
+
+            if (max == Math.sqrt(distance)) {
+                sb.append(max * 2 - 1).append("\n");
+            } else if (distance <= max * max + max) {
+                sb.append(max * 2).append("\n");
+            } else {
+                sb.append(max * 2 +1).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
