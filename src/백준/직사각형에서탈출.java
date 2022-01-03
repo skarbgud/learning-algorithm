4307ca7a1183645c846ca03aa9ce48eadf89b1da
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-03 09:08:23 +0900

    백준 1085번 직사각형에서 탈출

---
+++ src/백준/직사각형에서탈출.java
@@ -0,0 +1,25 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 1085번 직사각형에서 탈출
+class 직사각형에서탈출 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()+ " ");
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int x_min = Math.min(x, w-x);
+        int y_min = Math.min(y, h-y);
+
+        System.out.println(Math.min(x_min, y_min));
+    }
+}
