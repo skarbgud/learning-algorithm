eef92b868b1748fddb7d74c769e85e14d23b1710
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-08 20:49:30 +0900

    백준 7568번 덩치

---
+++ src/백준/덩치.java
@@ -0,0 +1,35 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 7568번 덩치
+class 덩치 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine() + " ");
+            arr[i][0] = Integer.parseInt(st.nextToken()); // 몸무게
+            arr[i][1] = Integer.parseInt(st.nextToken()); // 키
+        }
+
+        for (int i = 0; i < n; i++) {
+            int rank = 1;
+
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+            System.out.print(rank + " ");
+        }
+    }
+}
