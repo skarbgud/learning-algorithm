cba9b2b171327caa810186aa65424dd3a4507428
skarbgud <sjarbgud@gmail.com>
2022-02-24 09:12:44 +0900

    백준 1927번 최소힙

---
+++ src/백준/최소힙.java
@@ -0,0 +1,29 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+// 백준 1927번 최소힙
+class 최소힙 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> o1 - o2);
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (queue.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(queue.poll());
+                }
+            } else {
+                queue.offer(x);
+            }
+        }
+    }
+}
