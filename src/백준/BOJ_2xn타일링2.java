447ceb28a645bcd9e920482602be189445644e5e
GyuHyeong Nam <sjarbgud@gmail.com>
2022-04-04 12:34:02 +0900

    백준 11727번 2×n 타일링 2

---
+++ src/백준/BOJ_2xn타일링2.java
@@ -0,0 +1,23 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 백준 11727번 2×n 타일링 2
+class BOJ_2xn타일링2 {
+    static int[] dp = new int[1001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp[1] = 1;
+        dp[2] = 3;
+        dp[3] = 5;
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
