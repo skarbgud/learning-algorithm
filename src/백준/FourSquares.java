ba29e285dd8f71450ae5e944c2158d8c0b03da87
GyuHyeong Nam <sjarbgud@gmail.com>
2022-04-05 00:41:01 +0900

    백준 17626번 Four Squares

---
+++ src/백준/FourSquares.java
@@ -0,0 +1,28 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 백준 17626번 Four Squares
+class FourSquares {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[50001];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, dp[i - j * j]);
+            }
+            dp[i] = min + 1;
+        }
+        System.out.println(dp[n]);
+    }
+}
