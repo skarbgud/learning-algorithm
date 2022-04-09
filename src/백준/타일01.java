80fa97c069f0773fe371fd82b9e0fda7fc7f1450
GyuHyeong Nam <sjarbgud@gmail.com>
2022-04-10 01:57:20 +0900

    백준 1904번 01타일

---
+++ src/백준/타일01.java
@@ -0,0 +1,27 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+// 백준 1904번 01타일
+class 타일01 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1000001];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] %= 15746;
+        }
+        System.out.println(dp[n]);
+    }
+}
