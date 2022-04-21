02c09b315da9e3e0305c57a3d84c88b29458201e
GyuHyeong Nam <sjarbgud@gmail.com>
2022-04-21 21:59:40 +0900

    [Gold IV] Title: 집합의 표현, Time: 1564 ms, Memory: 74552 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 1717번 집합의 표현
+class Main {
+    static int arr[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine() + " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (command == 0) {
+                union(a, b);
+            } else {
+                unionFind(a, b);
+            }
+        }
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a != b) {
+            if (a < b) {
+                arr[b] = a;
+            } else {
+                arr[a] = b;
+            }
+        }
+    }
+
+    private static int find(int x) {
+        if (x == arr[x]) {
+            return x;
+        }
+        return arr[x] = find(arr[x]);
+    }
+
+    private static void unionFind(int a, int b) {
+        if(find(a) == find(b)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
