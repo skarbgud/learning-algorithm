69d01b0ff2b6ca24ddce2667527958b164247025
GyuHyeong Nam <sjarbgud@gmail.com>
2025-03-03 02:51:46 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] result = new int [num2 - num1 + 1];
+        
+        int index = 0;
+        
+        for (int i = num1; i <= num2; i++) {
+            result[index++] = numbers[i];
+        }
+        return result;
+    }
+}
